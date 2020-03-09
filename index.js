d8935a02821355f794cf123f2a981153b5dc67f0
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-10 09:46:09 +1300

    <C:/Program Files/Git/>

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
-console.log( +true);
-console.log( +false);
-console.log( +"");
+// console.log( +true);
+// console.log( +false);
+// console.log( +"");
 
-console.log(1+2, 3+4);
-console.log(5+6, 7+8);
-console.log(8+9, 10+11);
+// console.log(1+2, 3+4);
+// console.log(5+6, 7+8);
+// console.log(8+9, 10+11);
 
-console.log("Comma operator in a variable");
-let a =(1+2, 3+4);
-console.log(a);
+// console.log("Comma operator in a variable");
+// let a =(1+2, 3+4);
+// console.log(a);
 
-let x = 10;
-console.log(x);
-let y = 20;
-console.log(y);
+// let x = 10;
+// console.log(x);
+// let y = 20;
+// console.log(y);
 
-x = y;
-console.log (x);
+// x = y;
+// console.log (x);
 
-x+= y;
-console.log(x);
+// x+= y;
+// console.log(x);
 
-x -= y;
-console.log(x);
+// x -= y;
+// console.log(x);
 
-x *= y;
-console.log(x);
+// x *= y;
+// console.log(x);
 
-x /= y;
-console.log(x);
+// x /= y;
+// console.log(x);
 
-x %= y
-console.log(x)
+// x %= y
+// console.log(x)
 
+
+// // x == y;
+// // console.log (x);
+
+// // x===y;
+// // console.log(x);
+
+
+// let a =10;
+// let b =20;
+
+
+// if (a==b) {
+// console.log('same-numbers')
+// }
+
+console.log(10 == 5); //equal too
+console.log(10!= 5) //not equal too
+console.log(10 < 5); //less than
+console.log(10>5); //greater than
+console.log(10 <= 5) //less than or equal too
+console.log(10 >= 5)//Greater than or equal too
 
