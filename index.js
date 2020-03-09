874347121a03b2f643c12bbb0965c4c4265a78f7
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-10 09:16:16 +1300

    comma

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log( +true);
+console.log( +false);
+console.log( +"");
+
+console.log(1+2, 3+4);
+console.log(5+6, 7+8);
+console.log(8+9, 10+11);
+
+console.log("Comma operator in a wariable");
+let a =(1+2, 3+4);
+console.log(a);
