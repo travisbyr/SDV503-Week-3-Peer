cdc630dd53d10e5601f39780403cd3b24568df76
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-10 09:27:53 +1300

    x/y

--- index.js
+++ index.js
@@ -6,6 +6,31 @@ console.log(1+2, 3+4);
 console.log(5+6, 7+8);
 console.log(8+9, 10+11);
 
-console.log("Comma operator in a wariable");
+console.log("Comma operator in a variable");
 let a =(1+2, 3+4);
 console.log(a);
+
+let x = 10;
+console.log(x);
+let y = 20;
+console.log(y);
+
+x = y;
+console.log (x);
+
+x+= y;
+console.log(x);
+
+x -= y;
+console.log(x);
+
+x *= y;
+console.log(x);
+
+x /= y;
+console.log(x);
+
+x %= y
+console.log(x)
+
+
