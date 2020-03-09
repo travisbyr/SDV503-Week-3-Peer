4c73594a3613b720213e9e64a0253939181b51df
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-10 11:29:10 +1300

    cry

--- index.js
+++ index.js
@@ -189,3 +189,23 @@
     console.log(true || false);
     console.log(false || true);
     console.log(false || false);
+
+    //------------------------------------------
+
+    console.log(1 || 1);
+    console.log(1 || 0);
+    console.log(0 || 1);
+    console.log(0 || 0);
+
+    console.log(true || 0);
+    console.log(1 || false);
+    console.log(true || 1);
+    console.log(false || 1);
+    console.log(false || 0);
+    console.log(0 || 0);
+
+    //------------------------------------------
+    
+
+
+
